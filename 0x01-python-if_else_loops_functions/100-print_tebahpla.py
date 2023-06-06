d5d6bd62379c80f8781ba9dafd0f510d419bc332
kinoansah <kinoansah@gmail.com>
2023-06-06 07:41:46 -0700

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def reverse_alphabet():
-    for i in range(25, -1, -1):
-        if i % 2 == 0:
-            print(f"{chr(97 + i)}{chr(65 + i)}", end=" ")
-        else:
-            print(f"{chr(65 + i)}{chr(97 + i)}", end=" ")
-
-reverse_alphabet()
-
+for i in range(ord('z'), ord('a') - 1, -1):
+    print(chr(i).upper( ) if i % 2 == 0 else chr(i).lower( ), end='')
