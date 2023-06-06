e7359c6e66622b826bab46837d349e491dc1d322
kinoansah <125514115+kinoansah@users.noreply.github.com>
2023-06-06 17:41:32 +0000

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,54 +2,27 @@
 #include "lists.h"
 #include <stdlib.h>
 
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+listint_t *insert_node(listint_t **head, int num)
+{
+	listint_t *node = *head, *new;
 
-listint_t *insert_node(listint_t **head, int number) {
-    listint_t *new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL) {
-        return NULL;
-    }
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = num;
 
-    new_node->n = number;
-    new_node->next = NULL;
+	if (node == NULL || node->n >= num)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
 
-    if (*head == NULL || number < (*head)->n) {
-        new_node->next = *head;
-        *head = new_node;
-        return new_node;
-    }
+	while (node && node->next && node->next->n < num)
+		node = node->next;
 
-    listint_t *current = *head;
-    while (current->next != NULL && number > current->next->n) {
-        current = current->next;
-    }
+	new->next = node->next;
+	node->next = new;
 
-    new_node->next = current->next;
-    current->next = new_node;
-    return new_node;
+	return (new);
 }
-
-void print_list(listint_t *head) {
-    while (head != NULL) {
-        printf("%d ", head->n);
-        head = head->next;
-    }
-    printf("\n");
-}
-
-int main() {
-    listint_t *head = NULL;
-    insert_node(&head, 10);
-    insert_node(&head, 20);
-    insert_node(&head, 30);
-    insert_node(&head, 15);
-    insert_node(&head, 25);
-
-    print_list(head);
-
-    return 0;
-}
-
