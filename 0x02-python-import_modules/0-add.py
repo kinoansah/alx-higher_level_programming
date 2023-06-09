ae637411b8040892365645dfc7404108fd469f39
kinoansah <kinoansah@gmail.com>
2023-06-09 02:12:16 -0700

    Correction for Task 1 - Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 a = 1
 b = 2
-
-add_0 = __import__('add_0').add
-result = add_0(a, b)
-
-print(f"{a} + {b} = {result}")
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
