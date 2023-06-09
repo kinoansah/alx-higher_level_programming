6044992207b1c80a720a8505aee75a4f2b28c236
kinoansah <kinoansah@gmail.com>
2023-06-09 02:58:04 -0700

    Correction of Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-import dis
 
-with open('hidden_4.pyc', 'rb') as file:
-    code = file.read()
+if __name__ == "__main__":
+    '''Print all names defined by hidden_4 module.'''
+    import hidden_4
 
-names = []
-instructions = dis.get_instructions(code)
-for instruction in instructions:
-    if (
-        instruction.opname == 'LOAD_CONST'
-        and isinstance(instruction.argval, str)
-        and not instruction.argval.startswith('__')
-    ):
-        names.append(instruction.argval)
-
-for name in sorted(names):
-    print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
