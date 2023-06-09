8535123490c2473e32a607052370ecb9179eb179
kinoansah <kinoansah@gmail.com>
2023-06-09 02:51:50 -0700

    Correction of Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-import sys
-result = sum(int(arg) for arg in sys.argv[1:])
-print(result)
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    result = sum(int(arg) for arg in args)
+    print(result)
