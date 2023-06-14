bf9b2c95ddbc85786755a62299e1edcfc67d1ebe
kinoansah <kinoansah@gmail.com>
2023-06-14 13:14:32 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,18 +1,14 @@
 #!usr/bin/python3
 def best_score(a_dictionary):
-    # Check if the dictionary is empty or None
-    if not a_dictionary:
+    if a_dictionary is None:
         return None
 
-    # Initialize the maximum score and corresponding key
-    max_score = float('-inf')
-    best_key = None
+    max_key = None
+    max_value = float('-inf')
 
-    # Iterate over the key-value pairs in the dictionary
     for key, value in a_dictionary.items():
-        # Check if the current value is greater than the maximum
-        if value > max_score:
-            max_score = value
-            best_key = key
+        if value > max_value:
+            max_key = key
+            max_value = value
 
-    return best_key
+    return max_key
