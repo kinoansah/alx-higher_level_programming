561c252006551e02d4166607f99f0682374c8edd
kinoansah <kinoansah@gmail.com>
2023-07-01 06:57:51 -0700

    Max integer - Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,30 @@
+import unittest
+from max_integer import max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_regular_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_reversed_list(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_single_element_list(self):
+        self.assertEqual(max_integer([42]), 42)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-10, 0, 10]), 10)
+
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([3.14, 2.718, 1.618]), 3.14)
+
+
+if __name__ == '__main__':
+    unittest.main()
