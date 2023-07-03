d7019b3a14de6b97ed63c9f2eb793d8de8455ed5
kinoansah <kinoansah@gmail.com>
2023-07-03 10:12:03 -0700

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+Module 0-square
+Defines a square class that does nothing
+"""
+
+
 class Square:
     """
     This is an empty class that represents a square.
     """
-
     pass
