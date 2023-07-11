4d435a57a0fd1d75df889b18a4d060676c546c06
kinoansah <kinoansah@gmail.com>
2023-07-11 07:48:46 -0700

    Correction - Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""This program returns a list with the methods of an obj"""
+
+
 def lookup(obj):
-
     """Return the list of available attributes and methods of an object."""
-
     return dir(obj)
