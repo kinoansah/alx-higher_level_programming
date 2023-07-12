5eceaed8ea72d3c4a253f68680f238aacff69f2b
kinoansah <kinoansah@gmail.com>
2023-07-12 06:21:42 -0700

    Correction - Can I?

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""
+This program creates a Square from a Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
 class Square(Rectangle):
+    """Class Squared based from Rectangle"""
     def __init__(self, size):
-        self.__size = size
+        """Constructor of Square"""
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
-    def __str__(self):
-        """Return the string representation of the square."""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+    def area(self):
+        """Calculate the area of a Square"""
+        return self.__size ** 2
