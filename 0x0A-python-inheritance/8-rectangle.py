ae9060355a17169adb3778e393b5a1c04f1548e2
kinoansah <kinoansah@gmail.com>
2023-07-12 05:38:55 -0700

    Correction - Rctangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-from base_geometry import BaseGeometry
-from rectangle import Rectangle
 
-r = Rectangle(3, 5)
+"""
+A module for BaseGeometry class.
+"""
 
-print(r)  # Output: <rectangle.Rectangle object at 0x7f6f488f7eb8>
-print(dir(r))  # Output: List of attributes and methods of the Rectangle object
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-try:
-    print("Rectangle: {} - {}".format(r._Rectangle__width, r._Rectangle__height))  # Output: [AttributeError] 'Rectangle' object has no attribute 'width'
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    r2 = Rectangle(4, True)  # Output: [TypeError] height must be an integer
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+class Rectangle(BaseGeometry):
+    """A rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize the rectangle class."""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
