fd3fad4dd8027a01caaf217bf38deea166f08502
kinoansah <kinoansah@gmail.com>
2023-07-12 04:42:59 -0700

    Correction - Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-class BaseGeometry:
+"""
+class BaseGeometry based on 5-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    """
+    public instance, that raises an exception
+    """
     def area(self):
         """Calculate the area. This method is not implemented."""
         raise Exception("area() is not implemented")
