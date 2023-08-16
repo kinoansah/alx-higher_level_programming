f1a0af3b192ea8ff7f34dbe05bf6e8692220a638
kinoansah <kinoansah@gmail.com>
2023-08-16 12:15:33 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,10 +1,4 @@
--- Create the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
--- Create the user and grant privileges
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges
-FLUSH PRIVILEGES;
-
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
