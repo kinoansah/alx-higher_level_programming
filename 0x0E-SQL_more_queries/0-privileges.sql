d93afff6c9e55cc6bece54a972e41ea8059bf7ca
kinoansah <kinoansah@gmail.com>
2023-08-16 11:15:58 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,13 +1,3 @@
--- Create users
-CREATE USER 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
-
--- Grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Grant privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
 -- Show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
