73ee334bcdef0045e763bab38ea8a42fbbb3d3c1
kinoansah <kinoansah@gmail.com>
2023-09-15 22:28:21 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ This script lists all states from the
 database `hbtn_0e_0_usa`.
 """
 
-import mysql.connector
+import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
@@ -17,9 +17,9 @@ if __name__ == '__main__':
 
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states"
+    db_cursor.execute("SELECT * FROM states")
 
-            rows_selected = db_cursor.fetchall()
+    rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
         print(row)
