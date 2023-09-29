c916814f3ec62dd7fc3b84beddb84414a345b4e2
kinoansah <kinoansah@gmail.com>
2023-09-29 08:53:39 -0700

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# A python script that fetches
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
