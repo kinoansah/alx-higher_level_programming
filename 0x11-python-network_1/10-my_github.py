fa6761c075403dcd44d77f64d9f68fdbdbf9af14
kinoansah <kinoansah@gmail.com>
2023-10-02 12:46:16 -0700

    Correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,37 +1,18 @@
 #!/usr/bin/python3
-# A script that takes GitHub credentials and uses the GitHub API to display your id
-import requests
-import sys
+"""
+Takes your GitHub credentials (username and password) and uses the GitHub API
+to display your id
+"""
 
-def get_github_id(username, password):
-    # Define the GitHub API URL to fetch user details
-    api_url = f'https://api.github.com/user'
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-    # Set up the Basic Authentication header with your username and personal access token
-    auth = (username, password)
+    username = argv[1]
+    password = argv[2]
 
-    # Send a GET request to the GitHub API
-    response = requests.get(api_url, auth=auth)
+    URL = "https://api.github.com/user"
+    response = get(URL, auth=(username, password))
+    json = response.json()
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the JSON response to get your GitHub ID
-        user_data = response.json()
-        return user_data.get('id')
-    else:
-        # If authentication fails, return None
-        return None
-
-if __name__ == "__main__":
-    # Check for the correct number of command-line arguments
-    if len(sys.argv) != 3:
-        print("Usage: python 10-my_github.py <username> <personal_access_token>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-
-    github_id = get_github_id(username, password)
-
-    # Display the GitHub ID or None if authentication fails
-    print(github_id)
+    print(json.get('id'))
