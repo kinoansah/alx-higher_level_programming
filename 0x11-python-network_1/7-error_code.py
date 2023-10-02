f9cc01f653390ebfa58d768800a909b48bb491c5
kinoansah <kinoansah@gmail.com>
2023-10-02 12:44:28 -0700

    Correction

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-# A Python script that takes in a URL
-import requests
-import sys
+"""Sends a request to the URL and displays the body of the response."""
 
-if len(sys.argv) != 2:
-    print("Usage: ./7-error_code.py <URL>")
-    sys.exit(1)
 
-url = sys.argv[1]
+if __name__ == '__main__':
+    from sys import argv
+    from requests import get
 
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for HTTP errors
+    url = argv[1]
 
-    # Check if the HTTP status code is greater than or equal to 400
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
-    else:
-        # Print the response body
-        print(response.text)
-
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {str(e)}")
+    response = get(url)
+    ERR_TXT = 'Error code: {}'
+    status = response.status_code
+    print(ERR_TXT.format(status) if (status >= 400) else response.text)
