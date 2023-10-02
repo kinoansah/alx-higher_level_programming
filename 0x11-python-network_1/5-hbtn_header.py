c1dd307f94ea12b6fb2a3aa8bfa31e16a641ab2e
kinoansah <kinoansah@gmail.com>
2023-10-02 12:42:28 -0700

    Corrections

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
-# A Python script that takes in a URL, sends a request to the URL and displa
-import requests
-import sys
+"""
+Sends a request to the URL and displays the value of the variable X-Request-Id
+in the response header"""
 
-if len(sys.argv) != 2:
-    print("Usage: ./5-hbtn_header.py <URL>")
-    sys.exit(1)
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-url = sys.argv[1]
-
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-
-    # Check if the 'X-Request-Id' header is present in the response
-    if 'X-Request-Id' in response.headers:
-        x_request_id = response.headers['X-Request-Id']
-        print(x_request_id)
-    else:
-        print("X-Request-Id header not found in the response.")
-
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {str(e)}")
+    res = get(argv[1])
+    print(res.headers.get('X-Request-Id'))
